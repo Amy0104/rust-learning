ac1be004144342d8deae495bbd4310b1bba6827c
Amy0104 <amywang0104@163.com>
2022-07-07 15:02:37 +0800

    Add error and args.

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,15 +1,28 @@
+use std::env;
 use std::fs;
 
-fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: url outpath");
+        return Ok(());
+    }
+
+    args.iter().for_each(|arg| {
+        println!("{}", arg);
+    });
+
+    let url = &args[1];
+    let output = &args[2];
 
     println!("Fetching url:{}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("Converted markdown has been saved in {}", output);
+
+    Ok(())
 }
